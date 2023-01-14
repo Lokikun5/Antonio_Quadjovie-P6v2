c5170564c27f1871d4a5b5c33c12f7ed95d58af0
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2023-01-15 00:08:53 +0100

    mdr

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -13,8 +13,10 @@ async function getPhotographerId() {
   const photographers  = data.photographers;
   const medias = data.media;
   //- Récupere les infos photographe
-  const photographer = photographers.find(p => p.id === getUrlId);
-  const mediasPhotographer = medias.filter(m => m.photographerId === getUrlId);
+  // eslint-disable-next-line
+  const photographer = photographers.find(p => p.id == getUrlId);
+  // eslint-disable-next-line
+  const mediasPhotographer = medias.filter(m => m.photographerId == getUrlId);
   mediaSave = mediasPhotographer
   //- afficher les infos photographe header
   document.getElementById('name').innerText = photographer.name;
